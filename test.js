7485b310e51846a5d4d63182a000904988c63d5b
Emeraude <broggi_t@epitech.eu>
2014-12-08 21:29:01 +0100

    update tests

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 //console.log("Only works if you've done `npm install mhash`");
-//var hash = require("mhash").hash;
+//var hash = require("mhash");
 
-var hash = require("./index").hash;
+var hash = require("./index");
 console.log(hash("md2", "testing"));
 
 console.log("\nNext should be: 0c1e02ff");
